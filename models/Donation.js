f595213078fbb6974fa6e7fb5d00cabe6c118094
Vanshika Gupta <gvanshika844@gmail.com>
2025-07-22 14:26:02 +0530

    Update Donation.js

--- models/Donation.js
+++ models/Donation.js
@@ -6,10 +6,13 @@ const donationSchema = new mongoose.Schema({
   email: String,
   sevaType: String,
   amount: Number,
+  quantity: Number, // 👈 (if you’re using seva quantity)
   paymentProofUrl: String,
   donationDate: { type: Date, default: Date.now },
   receiptSent: { type: Boolean, default: false },
   proofSent: { type: Boolean, default: false },
+  received80G: { type: Boolean, default: false }, // 👈 ADD THIS LINE
+  proofLink: String, // 👈 optional: to store 80G folder link
   notes: String
 });
 
