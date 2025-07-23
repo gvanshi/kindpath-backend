98eb42573e0c53218372ff0fcfad1f795afb7937
Vanshika Gupta <gvanshika844@gmail.com>
2025-07-23 16:38:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,22 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const donationRoutes = require('./routes/donationRoutes');
 const Donation = require('./models/Donation'); // 🛑 You were missing this
+const Receipt80G = require('../models/receipt80g');
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+router.post('/api/receipts80g', async (req, res) => {
+  try {
+    const receipt = new Receipt80G(req.body);
+    await receipt.save();
+    res.status(201).json({ message: 'Receipt saved successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Something went wrong', details: err });
+  }
+});
 app.use('/api/donations', donationRoutes);
 
 // ✅ FIXED DELETE route directly in app
