bc1255d292a20f48f03b2dd90c3cd2a65aa8a23f
Vanshika Gupta <gvanshika844@gmail.com>
2025-08-22 16:28:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 
 const corsOptions = {
-  origin: ['https://kindpath-frontened-crmon.vercel.app'], // Add your frontend domains here
+  origin: ['https://kindpath-frontened-crmon.vercel.app', 'http://127.0.0.1:5500'], // Add your frontend domains here
   credentials: true
 };
 
